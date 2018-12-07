9ab7437f7f070b204115d4c70c1e085c3e6bf4e8
John Blanchard <jnblanchard@mac.com>
2018-12-07 09:28:18 -0800

    Initial Commit

---
+++ MoeBamba/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoeBamba
+//
+//  Created by John N Blanchard on 12/7/18.
+//  Copyright © 2018 JNB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
